67f9867562d957764fd1ec7c4fb0673badf5d27d
Archer <thivan.nms@gmail.com>
2022-12-13 07:51:46 +0700

    fix enter still work unintentional

--- tasks.js
+++ tasks.js
@@ -3,19 +3,31 @@ document.addEventListener('DOMContentLoaded', () => {
     // Set variables
     let input = document.querySelector('#task');
     let submit = document.querySelector('#submit');
-    let tasks = document.querySelector('#tasks');
+    let tasks = document.querySelector('#tasks');    
 
-    // By default, submit button is disable
+    // Define function that disable Enter key in the form
+    function dis_Enter(e) {
+        if(e.keyCode === 13 || e.which === 13) {
+            e.preventDefault();
+            return false;
+        }
+    }
+
+    // By default, submit button is disable, Enter is also disable
     submit.disabled = true;
+    input.addEventListener('keypress', dis_Enter)
 
+    // When user type something in input, make submit button and Enter key to work again if condition met
     input.onkeyup = () => {
 
         // If the input don't have any character, also use trim to eliminate space
         if (input.value.trim().length > 0) {
             submit.disabled = false;
-        }
-        else {
+            input.removeEventListener('keypress', dis_Enter);
+
+        } else {
             submit.disabled = true;
+            input.addEventListener('keypress', dis_Enter);
         }        
     }
 
@@ -30,17 +42,10 @@ document.addEventListener('DOMContentLoaded', () => {
         input.value = '';
 
         submit.disabled = true;
+        input.addEventListener('keypress', dis_Enter);
 
         // stop form from submitting
         return false;
     }
-
-    // Disable Enter key for this form's input
-    input.addEventListener('keypress', function(e) {
-        if (e.keyCode === 13 || e.which === 13) {
-            e.preventDefault();
-            return false;
-        }
-    })
 
 })
